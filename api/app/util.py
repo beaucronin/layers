344ee0db895a177e1809ec59c0928b17e98751c0
Beau Cronin <beau.cronin@gmail.com>
2023-01-08 23:13:55 +0000

    pull lat lng from data field

--- api/app/util.py
+++ api/app/util.py
@@ -23,14 +23,28 @@ def extract_place_info(url):
     
     # Extract the place name and latitude and longitude from the path components
     name = bc.title(urllib.parse.unquote(path_components[3]).replace('+', ' '))
-    lat_lng_string = path_components[4]
-    lat_lng_match = re.search(r'@?([\d.-]+),([\d.-]+)', lat_lng_string)
-    if lat_lng_match:
-        lat = float(lat_lng_match.group(1))
-        lng = float(lat_lng_match.group(2))
-    else:
-        lat = None
-        lng = None
+    data_string = path_components[5]
+    data_fields = data_string.split('!')
+    lat = None
+    lng = None
+    for df in data_fields:
+        if df.startswith('3d'): 
+            # latitude
+            lat = float(df[2:])
+        elif df.startswith('4d'):
+            # longitude
+            lng = float(df[2:])
+
+    if lat is None or lng is None:
+        # Try to extract the latitude and longitude from the path
+        lat_lng_string = path_components[4]
+        lat_lng_match = re.search(r'@?([\d.-]+),([\d.-]+)', lat_lng_string)
+        if lat_lng_match:
+            lat = float(lat_lng_match.group(1))
+            lng = float(lat_lng_match.group(2))
+        else:
+            lat = None
+            lng = None
     
     # Return the place info as a dictionary
     return {'description': name, 'latitude': lat, 'longitude': lng}
